6b81c81a3417ba06e9c70977ef9d1fa37b24e291
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-10-25 20:32:59 +0600

    dynamic url

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ app.use(cors());
 const Port = process.env.Port || 5000;
 const courseDetails = require('./Data/course.json');
 
+app.get("/",(req,res)=> {
+    res.send(courseDetails);
+})
+
+app.get("/course/:id",(req,res)=> {
+    const id = req.params.id;
+    res.send(courseDetails.find(element => element.id == id ));
+})
+
 app.listen(Port, ()=> {
     console.log('server is running', Port);
 })
